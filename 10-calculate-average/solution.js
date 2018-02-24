543d49be65c28edf39c0c7809b4e6cd07b1e1089
Beverly Pham <beverly.pham@gmail.com>
2018-02-24 12:35:56 -0800

    solved

---
+++ 10-calculate-average/solution.js
@@ -0,0 +1,19 @@
+// they both work; debating on which one is more readable
+
+function find_average(array) {
+  let total = array.reduce((accumulator, current) => {
+    return accumulator + current;
+  }, 0);
+  return total / array.length;
+}
+
+function find_average(array) {
+  return (array.reduce((accumulator, current) => {
+    return accumulator + current;
+  }, 0)) / array.length;
+}
+
+// or in one line
+function find_average(array) {
+  return (array.reduce((accumulator, current) => accumulator + current, 0)) / array.length;
+}
